fa16ab8a4434cf28677e1b2d5a338602f04f5bb9
yuki0083 <gasintare212@gmail.com>
2021-11-30 19:05:31 +0900

    modified video name

--- draw_trajectory/video_processing_class.py
+++ draw_trajectory/video_processing_class.py
@@ -2,12 +2,14 @@ import cv2
 from pandas.core import frame
 from opencv_func import get_video_prop,  write_circle
 import sys
+import os
 
 class video_processing:
     def __init__(self,df):
         self.detected_df = df.df
         self.video_path = df.video_path
         self.video_dir = df.video_dir
+        self.video_name = os.path.basename(self.video_path).split(".")[0]
         self.video_W , self.video_H, self.video_count, self.video_fps = get_video_prop(self.video_path)
         
         
@@ -16,7 +18,7 @@ class video_processing:
         
         #動画の保存方法を指定(#TODO　動画の名前にtrajectoryをつける)
         fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')#MP4
-        writer = cv2.VideoWriter(self.video_dir + 'trajectory.mp4', fmt, self.video_fps, (self.video_W, self.video_H))
+        writer = cv2.VideoWriter(self.video_dir + self.video_name +'_trajectory.mp4', fmt, self.video_fps, (self.video_W, self.video_H))
 
         video = cv2.VideoCapture(self.video_path)
         """
