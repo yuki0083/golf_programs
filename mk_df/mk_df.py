1c89021b512da70605bdc4766bc35464f486c9aa
Yuki Yamada <gasintare212@gmail.com>
2022-01-07 13:54:48 +0900

    add df sort function

--- mk_df/mk_df.py
+++ mk_df/mk_df.py
@@ -32,7 +32,8 @@ def mk_detected_data_list(video_dir):
     detected_txt_list = glob.glob(video_dir+'labels/*.txt')
     for txt_path in detected_txt_list:
         frame_num = int((os.path.basename(txt_path).split('.')[0]).split('_')[-1])#txtファイルの名前からフレーム番号を取得
-        with open(txt_path) as f: #txtデータ[クラス, x, y, w, h, conf]
+        with open(txt_path) as f: #txtデータ[クラス, x, y, w, h, conf] 
+        #各クラスで確信度の最も高いものだけdfに保存
             detected_cls_dic = {}
             l = f.read().split("\n")
             for one_detect in l:
@@ -55,10 +56,16 @@ def mk_df(video_dir): #video_dir: runs/detect/expn/
     detected_data_list = mk_detected_data_list(video_dir)
     #2次元リストからデータフレームを作成
     df = pd.DataFrame(detected_data_list,columns=['frame_num', 'class', 'x', 'y', 'w', 'h', 'conf'] )
+    return df
+#dfをフレーム番号（クラス)の順に並び替える
+def sort_df(df):
+    df = df.sort_values(['frame_num', 'class'])
+    df = df.reset_index(drop=True)
+    return df
+
+def df_to_csv(df, video_dir):
     df.to_csv(video_dir + 'detected_df.csv')
     print("finish writing csv in {}".format(video_dir))
-
-
 
 
 
@@ -72,7 +79,10 @@ def main():
     video_dir_list = not_trajectory_path(project)
 
     for video_dir  in video_dir_list:
-        detected_data = mk_df(video_dir)
+        df = mk_df(video_dir)
+        df = sort_df(df)
+        df_to_csv(df, video_dir)
+
 
 if __name__ == '__main__':
     main()
